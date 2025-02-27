e8751b55ceb787149d8838a28f1e99b01ffe58c9
konabe <daiya.adgjmp1014@gmail.com>
2025-02-27 23:01:52 +0900

    環境準備

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path, { resolve } from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  root: '/src',
   plugins: [react()],
+  publicDir: resolve(__dirname, "public"),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  // build: {
+  //   outDir: resolve(__dirname, "dist"),
+  //   emptyOutDir: true,
+  //   copyPublicDir: true,
+  //   rollupOptions: {
+	// 		input: {
+	// 			"": resolve(__dirname, "src/index.html"),
+	// 		},
+	// 		output: {
+	// 			entryFileNames: "assets/bundle.js",
+	// 		},
+	// 	},
+  // }
 })
