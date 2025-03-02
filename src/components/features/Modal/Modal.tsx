31fa0d588ce55db2fb6a702a41eafa3b2438d2b0
konabe <daiya.adgjmp1014@gmail.com>
2025-03-02 20:26:55 +0900

    todo作成画面構築

--- src/components/features/Modal/Modal.tsx
+++ src/components/features/Modal/Modal.tsx
@@ -1,5 +1,6 @@
 import FormModal from "react-modal"
 import styles from "./Modal.module.scss"
+import { useEffect, useRef, useState } from "react";
 
 interface ModalProps {
 	isOpen: boolean
@@ -9,6 +10,25 @@ interface ModalProps {
 FormModal.setAppElement('#root');
 
 export default function Modal ({ isOpen, closeModal }: ModalProps) {
+  const [selectedType, setSelectedType] = useState("note")
+  const textRef = useRef<HTMLTextAreaElement>(null)
+
+  const handleSelectedChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setSelectedType(event.target.value)
+  }
+
+  const capitalizeFirstLetter = (str: string):string => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
+  const handleCreateTodo = () => {
+    console.log(textRef.current?.value, selectedType)
+  }
+
+  useEffect(() => {
+    console.log(selectedType)
+  }, [selectedType])
+
   return (
 		<FormModal
 			isOpen={isOpen}
@@ -17,8 +37,34 @@ export default function Modal ({ isOpen, closeModal }: ModalProps) {
 			overlayClassName={ styles.overlay }
 		>
 			<button onClick={closeModal} className={ styles.modalClose } type="button"></button>
-			<div className="modal-content">
-        サンプル
+			<div className={ styles.modalContent }>
+
+        {/* ラジオボタン */}
+        <div className={ styles.modalRadio }>
+          <input
+              type="radio"
+              value="note"
+              checked={selectedType === "note"}
+              onChange={handleSelectedChange}
+              id="note"
+            />
+          <label htmlFor="note" className={ selectedType === "note" ? styles.isChecked : "" }>Note</label>
+
+          <input
+            type="radio"
+            value="memo"
+            checked={selectedType === "memo"}
+            onChange={handleSelectedChange}
+            id="memo"
+          />
+          <label htmlFor="memo" className={ selectedType === "memo" ? styles.isChecked : "" }>Memo</label>
+        </div>
+
+        {/* テキストエリア */}
+        <textarea ref={textRef} className={ styles.modalTextArea } rows={6} placeholder={capitalizeFirstLetter(selectedType)} />
+
+        {/* 作成 */}
+        <button onClick={handleCreateTodo} className={ styles.modalSubmit } type="button">Create task</button>
       </div>
 		</FormModal>
 	)
